017b8f7c54793f5d5b215d5f1e1f6c0737cc238d
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2024-11-27 15:31:59 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <h1>Pagina Inicial</h1>
       <span>Lista dos Exercicios</span>
-      <li><a href="/aula1">Aula 1</a></li>
+      <li><a href="/aula1">Aula 1 Position Absolute/Relative</a></li>
       <li><a href="/aula2">Aula 2</a></li>
     </div>
   );
