987513acee6f64eb17209ba3828da3cf49d1fb85
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2024-11-27 15:53:18 -0300

    Adicionado Função de trocar de cor do background na Aula 1

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Pagina Inicial</h1>
+      <p>O objetivo desta página é aprender/praticar e deixar anotado codigos utéis de CSS e React com TypesScript</p>
       <span>Lista dos Exercicios</span>
       <li><a href="/aula1">Aula 1 Position Absolute/Relative</a></li>
       <li><a href="/aula2">Aula 2</a></li>
--- src/components/Aula1/Aula1.tsx
+++ src/components/Aula1/Aula1.tsx
@@ -1,15 +1,35 @@
+import { useState } from "react";
 import "./style.css";
 import { HiBackward } from "react-icons/hi2";
 import { HiForward } from "react-icons/hi2";
 
-
 export const Aula1 = () => {
+  interface Colors {
+    color: string;
+  }
+
+  const [index, setIndex] = useState(0);
+
+  const colors: Colors[] = [
+    { color: "red" },
+    { color: "orange" },
+    { color: "white" },
+  ];
+
+  function nextColor() {
+    setIndex((prev) => (prev + 1) % colors.length);
+  }
+
+  function previousColor() {
+    setIndex((prev) => (prev - 1 + colors.length) % colors.length);
+  }
+
   return (
     <div className="body">
-      <div className="box">
-        <HiBackward className="arrow left"/>
-        <HiForward className="arrow right"/>
+      <div className="box" style={{ backgroundColor: colors[index].color }}>
+        <HiBackward className="arrow left" onClick={previousColor} />
+        <HiForward className="arrow right" onClick={nextColor} />
       </div>
     </div>
-  )
-}
+  );
+};
