70371564b6143ac635ce6cb40aab5c5e30175746
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2024-11-27 15:31:23 -0300

    Bug no css

--- src/components/Aula1/Aula1.tsx
+++ src/components/Aula1/Aula1.tsx
@@ -5,7 +5,7 @@ import { HiForward } from "react-icons/hi2";
 
 export const Aula1 = () => {
   return (
-    <div className="App">
+    <div className="body">
       <div className="box">
         <HiBackward className="arrow left"/>
         <HiForward className="arrow right"/>
