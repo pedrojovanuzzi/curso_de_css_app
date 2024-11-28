c6b3059663555b3020b67849dbf8d9f1745ba0d6
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2024-11-28 12:04:24 -0300

    Aula 2 Feita
    Adicionado CheckBox com Css estilizado totalmente na mão, sem utilizar bibliotecas

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
       <h1>Pagina Inicial</h1>
       <p>O objetivo desta página é aprender/praticar e deixar anotado codigos utéis de CSS e React com TypesScript</p>
       <span>Lista dos Exercicios</span>
-      <li><a href="/aula1">Aula 1 Position Absolute/Relative</a></li>
-      <li><a href="/aula2">Aula 2</a></li>
+      <a href="/aula1">Aula 1 Position Absolute/Relative</a>
+      <a href="/aula2">Aula 2 Checkbox Puro com Css</a>
     </div>
   );
 }
--- src/components/Aula2/Aula2.tsx
+++ src/components/Aula2/Aula2.tsx
@@ -1,5 +1,26 @@
+import "./style.css";
+
 export const Aula2 = () => {
   return (
-    <div>Aula2</div>
-  )
-}
+    <div className="aula2_body">
+      <div className="check_div">
+        <h1>CheckBox</h1>
+        <label htmlFor="one">
+          <input type="checkbox" name="one" id="one" />
+          <span></span>
+          <a>One</a>
+        </label>
+        <label htmlFor="two">
+          <input type="checkbox" name="two" id="two" />
+          <span></span>
+          <a>Two</a>
+        </label>
+        <label htmlFor="three">
+          <input type="checkbox" name="three" id="three" />
+          <span></span>
+          <a>Three</a>
+        </label>
+      </div>
+    </div>
+  );
+};
