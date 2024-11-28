41e4c5ed379a0465c9647d433866fb3b76da827b
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2024-11-28 15:27:55 -0300

    Adicionado Acessibilidade Aula2

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <p>O objetivo desta página é aprender/praticar e deixar anotado codigos utéis de CSS e React com TypesScript</p>
       <span>Lista dos Exercicios</span>
       <a href="/aula1">Aula 1 Position Absolute/Relative</a>
-      <a href="/aula2">Aula 2 Checkbox Puro com Css</a>
+      <a href="/aula2">Aula 2 Checkbox Puro com Css e Acessibilidade</a>
     </div>
   );
 }
