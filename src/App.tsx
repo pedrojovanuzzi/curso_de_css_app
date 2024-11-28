8ffda099e8519356a0fdbf484592a797976f3d38
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2024-11-28 16:43:39 -0300

    Arrumado CSS, usando module  e sem module

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ function App() {
       <h1>Pagina Inicial</h1>
       <p>O objetivo desta página é aprender/praticar e deixar anotado codigos utéis de CSS e React com TypesScript</p>
       <span>Lista dos Exercicios</span>
-      <a href="/aula1">Aula 1 Position Absolute/Relative</a>
-      <a href="/aula2">Aula 2 Checkbox Puro com Css e Acessibilidade</a>
-      <a href="/aula3">Aula 3 Galeria de Imagens</a>
+      <a className='a_app' href="/aula1">Aula 1 Position Absolute/Relative</a>
+      <a className='a_app' href="/aula2">Aula 2 Checkbox Puro com Css e Acessibilidade</a>
+      <a className='a_app' href="/aula3">Aula 3 Galeria de Imagens</a>
     </div>
   );
 }
