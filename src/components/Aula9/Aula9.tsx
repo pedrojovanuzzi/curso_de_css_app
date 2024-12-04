c7f3192dede0050ebce40b29fd4a35015a0488de
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2024-12-04 16:20:33 -0300

    Aula 9 Finalizada com Tailwind

--- src/components/Aula9/Aula9.tsx
+++ src/components/Aula9/Aula9.tsx
@@ -1,7 +1,22 @@
+import { HiMagnifyingGlass, HiMicrophone } from "react-icons/hi2";
+
 export const Aula9 = () => {
   return (
-    <div className="flex justify-center">
-        <div>Aula9</div>
+    <div className="flex justify-center items-center h-screen bg-slate-800">
+      <div className="text-center w-screen">
+        <h1 className="text-white">Recriando o Buscar do Google</h1>
+        <div className="flex justify-center items-center mt-5">
+          <div className="relative flex items-center w-6/12">
+            <HiMagnifyingGlass className="absolute left-3 text-gray-500 cursor-pointer" />
+            <input
+              type="text"
+              placeholder="Buscar"
+              className="w-full pl-10 pr-10 py-2 bg-white text-sm font-medium text-slate-700 rounded-full focus:outline-none placeholder-gray-500"
+            />
+            <HiMicrophone className="absolute right-3 text-gray-500 cursor-pointer" />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
