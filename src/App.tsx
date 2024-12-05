7df0429451b28a475eba107b3af497f1ae5bffe7
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2024-12-05 12:05:22 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
       <a className='a_app' href="/aula8">Aula 8 NavBar Responsiva (Mobile First)</a>
       <a className='a_app' href="/aula9">Aula 9 Começando com TailWind</a>
       <a className='a_app' href="/aula10">Aula 10 TailWind Components</a>
+      <a className='a_app' href="/aula11">Aula 11 Criando um Site Completo</a>
     </div>
   );
 }
