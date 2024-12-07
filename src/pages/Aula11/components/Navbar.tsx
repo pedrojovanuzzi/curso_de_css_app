1d92855bd799308508def8a615bf68dc754727fa
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2024-12-07 10:11:49 -0300

    Update Navbar.tsx

--- src/pages/Aula11/components/Navbar.tsx
+++ src/pages/Aula11/components/Navbar.tsx
@@ -41,7 +41,7 @@ export default function NavBar() {
     <header className="bg-gray-900">
       <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+          <a href="/aula11_home" className="-m-1.5 p-1.5">
             <span className="sr-only">Wip Telecom</span>
             <img
               alt=""
